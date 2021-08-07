b10285bd8aaf5126361be3def659805d555c2261
“fanguangjian” <fanguangjianauto@163.com>
2021-08-08 00:33:43 +1000

    test

--- db/redis.js
+++ db/redis.js
@@ -1,7 +1,15 @@
+/*
+ * @Author: G.F
+ * @Date: 2021-08-08 00:27:27
+ * @LastEditTime: 2021-08-08 00:33:31
+ * @LastEditors: your name
+ * @Description: 
+ * @FilePath: /Node-Express/db/redis.js
+ */
 const redis = require('redis')
 const { REDIS_CONF } = require('../conf/db.js')
 
-// 创建客户端
+// 创建客户端  cc
 const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host)
 
 redisClient.on('error', err => {
